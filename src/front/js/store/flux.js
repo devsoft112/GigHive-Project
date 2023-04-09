985be5ac66087a8de8d40f4bcb0cae26467f284a
phorjax <joseph.12082@gmail.com>
2023-04-09 22:49:26 +0000

    fetch API created, needs to be tested

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -19,7 +19,7 @@ const getState = ({ getStore, getActions, setStore }) => {
           console.log("Error loading artists", error);
         }
       },
-      getVenues: async () => {
+      getVenue: async () => {
         try {
           // fetching data from the backend
           const resp = await fetch(process.env.BACKEND_URL + "/api/venues");
@@ -29,6 +29,64 @@ const getState = ({ getStore, getActions, setStore }) => {
           return data;
         } catch (error) {
           console.log("Error loading artists", error);
+        }
+      },
+      postArtist: async (
+        first_name,
+        last_name,
+        username,
+        email,
+        password,
+        artist_name,
+        genre,
+        performmance_type,
+        image,
+        instagram,
+        facebook,
+        twitter,
+        soundcloud,
+        spotify,
+        tiktok
+      ) => {
+        const opts = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            first_name: first_name,
+            last_name: last_name,
+            username: username,
+            email: email,
+            password: password,
+            artist_name: artist_name,
+            genre: genre,
+            performmance_type: performmance_type,
+            image: image,
+            instagram: instagram,
+            facebook: facebook,
+            twitter: twitter,
+            soundcloud: soundcloud,
+            spotify: spotify,
+            tiktok: tiktok,
+          }),
+        };
+        try {
+          const response = await fetch(
+            process.env.BACKEND_URL + "/api/register/artist",
+            opts
+          );
+          if (response.status !== 200) {
+            alert("Response was not a code 200.");
+            return false;
+          }
+          const data = await response.json();
+          console.log("backend token: " + data);
+          sessionStorage.setItem("user", data.access_token);
+          setStore({ token: data.access_token });
+          return true;
+        } catch (error) {
+          console.error("Error! Description: " + error);
         }
       },
     },
