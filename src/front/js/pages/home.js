ed991c0ae3b9c070009c7320fe5f1bfb34029f53
YanceMcFinn <fsdoubleflip@gmail.com>
2023-04-14 01:36:53 +0000

    updated banner, added styling to genre buttons. Also added onClick to buttons that should filter by genre once we have artists in the DB

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
 import { Artistcard } from "../component/artistscards";
@@ -12,37 +12,45 @@ import ClassicalMusic from "../../img/ClassicalMusic.png"
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
+  const [ artists, setArtists ] = useState(store.artists)
+
+  const rockFilter = () => setArtists(artists.filter(artist => artist.genre == "rock"));
+  const hipHopFilter = () => setArtists(artists.filter(artist => artist.genre == "hip hop"));
+  const jazzFilter = () => setArtists(artists.filter(artist => artist.genre == "jazz"));
+  const electronicFilter = () => setArtists(artists.filter(artist => artist.genre == "electronic"));
+  const classicalFilter = () => setArtists(artists.filter(artist => artist.genre == "classical"));
+
 
   return (
     <div className="container-fluid">
 
       {/* EVERYTHING ABOVE THIS LINE FOR HEADER TESTS */}
-      <div className="row header-image p-3"><h1 className="header-text">Find<br></br>Your Crowd</h1></div>
-      <div className="row text-center">
+      <div className="row header-image p-3 align-items-center"><h1 className="header-text text-center">Where <b>Music</b> Meets <b>Opportunity</b></h1></div>
+      <div className="row text-center mt-2">
         <h1>Bringing Musicians and Venues Together!</h1>
       </div>
       <div className="row text-center">
         <h4>Genres</h4>
       </div>
       <div className="genre-list row d-flex justify-content-center px-3">
-          <div className="genre rock d-flex rounded-circle justify-content-center mx-1">
-            <img className="genreImage" src={rockMusic} />
+          <div className="genre d-flex rounded-circle justify-content-center mx-1">
+            <img className="genreImage" src={rockMusic} onClick={rockFilter} />
             <p className="genreText">Rock</p>
           </div>
-          <div className="genre rock d-flex rounded-circle justify-content-center mx-1">
-            <img className="genreImage" src={HipHopMusic} />
+          <div className="genre d-flex rounded-circle justify-content-center mx-1">
+            <img className="genreImage" src={HipHopMusic} onClick={hipHopFilter} />
             <p className="genreText">Hip-Hop</p>
           </div>
-          <div className="genre rock d-flex rounded-circle justify-content-center mx-1">
-            <img className="genreImage" src={JazzMusic} />
+          <div className="genre d-flex rounded-circle justify-content-center mx-1">
+            <img className="genreImage" src={JazzMusic} onClick={jazzFilter} />
             <p className="genreText">Jazz</p>
           </div>
-          <div className="genre rock d-flex rounded-circle justify-content-center mx-1">
-            <img className="genreImage" src={ElectronicMusic} />
+          <div className="genre d-flex rounded-circle justify-content-center mx-1">
+            <img className="genreImage" src={ElectronicMusic} onClick={electronicFilter} />
             <p className="genreText">Electronic</p>
           </div>
-          <div className="genre rock d-flex rounded-circle justify-content-center mx-1">
-            <img className="genreImage" src={ClassicalMusic} />
+          <div className="genre d-flex rounded-circle justify-content-center mx-1">
+            <img className="genreImage" src={ClassicalMusic} onClick={classicalFilter} />
             <p className="genreText">Classical</p>
           </div>
           <div className="genre other d-flex rounded-circle justify-content-center mx-1">
@@ -57,17 +65,17 @@ export const Home = () => {
           <Artistcard />
         </div>
 
-      <section>
+      <div className="row px-3 mt-3">
         <h2>Venues</h2>
         <div className="d-flex flex-row flex-nowrap overflow-auto">
           <Venuecard />
         </div>
-      </section>
+      </div>
 
-      <div className="alert alert-info">
+      {/* <div className="alert alert-info">
         {store.message ||
           "Loading message from the backend (make sure your python backend is running)..."}
-      </div>
+      </div> */}
     </div>
   );
 };
