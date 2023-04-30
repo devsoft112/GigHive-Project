a1522471b213b2170eca767e69d8da284ddfe0f1
YanceMcFinn <fsdoubleflip@gmail.com>
2023-04-30 00:19:22 +0000

    minor db fix

---
+++ migrations/versions/0a1262b176df_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 0a1262b176df
+Revises: 2f8002878ee1, afdec6e57a94
+Create Date: 2023-04-30 00:18:22.286309
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0a1262b176df'
+down_revision = ('2f8002878ee1', 'afdec6e57a94')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
