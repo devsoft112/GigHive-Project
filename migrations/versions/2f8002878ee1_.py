dfa1377c6c7c45ceecb25f5bd75ba62ed78f9e8f
YanceMcFinn <fsdoubleflip@gmail.com>
2023-04-30 03:05:02 +0000

    testing migration fix

---
+++ migrations/versions/2f8002878ee1_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 2f8002878ee1
+Revises: afdec6e57a94
+Create Date: 2023-04-30 03:03:58.255783
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2f8002878ee1'
+down_revision = 'afdec6e57a94'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
