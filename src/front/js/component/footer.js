0df2ff2759a444bf3a4607d281245f62ad9d74b6
YanceMcFinn <fsdoubleflip@gmail.com>
2023-05-12 03:15:25 +0000

    one last thing i swear

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -4,7 +4,7 @@ export const Footer = () => (
   <footer className="footer mt-auto py-3 text-center">
     <p>
       Made with <i className="fa fa-heart text-danger" /> by{" "}
-      <a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+      <a href="https://github.com/phorjax" target="_blank">Anthony G.</a>, <a href="https://github.com/WyattChase" target="_blank">Jean S.</a>, <a href="https://github.com/Sierra71999" target="_blank"> Sierra E.</a>, and <a href="https://github.com/YanceMcFinn" target="_blank">Sean C.</a>
     </p>
   </footer>
 );
